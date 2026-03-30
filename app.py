ee3882fa0db6874d2848e09269270e3e4b38629f
vinicius42-coder <vinicius.amorim230@icloud.com>
2026-03-30 13:41:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import pandas as pd
 
 df = pd.read_csv("dados.csv")
 
-st.title("Dashboard Logístico")
-
+st.title("📊 Dashboard de Produtividade Logística")
+st.markdown("Análise de desempenho operacional em tempo real")
 st.dataframe(df)
 
 st.subheader("Resumo")
-st.write("Total de pedidos:", df["qtnd"].sum())
-st.write("Tempo médio:", df["time"].mean())
+col1, col2 = st.columns(2)
 
+with col1:
+    st.metric("Total de pedidos", df["qtnd"].sum())
+
+with col2:
+    st.metric("Tempo médio", round(df["time"].mean(), 2))
+  
 st.subheader("Pedidos por Funcionário")
 st.bar_chart(df.groupby("name")["qtnd"].sum())
